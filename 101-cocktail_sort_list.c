b90d9443cbce73cd9d6f212750d77c7959bb21ff
Alfred Ndlovu <72683354+Freddy-cod@users.noreply.github.com>
2024-08-10 15:49:34 +0200

    Update 101-cocktail_sort_list.c

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,46 +1,47 @@
 #include "sort.h"
 
 /**
- * swap_nodes - Swaps two adjacent nodes in a doubly linked list
- * @list: Double pointer to the head of the list
- * @left: The node to swap with the next node
- * @right: The next node to swap with the left node
+ * swap_nodes - swaps two adjacent nodes in a doubly linked list
+ * @list: pointer to the head of the list
+ * @left: pointer to the left node
+ * @right: pointer to the right node
  */
 void swap_nodes(listint_t **list, listint_t *left, listint_t *right)
 {
+  if (!list || !left || !right || left == right)
+    return;
+
+  left->next = right->next;
+  if (right->next)
+    right->next->prev = left;
+
+  right->prev = left->prev;
   if (left->prev)
     left->prev->next = right;
   else
     *list = right;
-  
-  if (right->next)
-    right->next->prev = left;
-  
-  left->next = right->next;
-  right->prev = left->prev;
-  left->prev = right;
-  right->next = left;
 
-  print_list(*list);
+  right->next = left;
+  left->prev = right;
 }
 
 /**
- * cocktail_sort_list - Sorts a doubly linked list
- * @list: Double pointer to the head of the list
+ * cocktail_sort_list - sorts a doubly linked list
+ * @list: pointer to the head of the list
  */
 void cocktail_sort_list(listint_t **list)
 {
   int swapped = 1;
-  listint_t *start = *list;
-  listint_t *end = NULL;
+  listint_t *start, *end = NULL;
 
-  if (!list || !*list)
+  if (!list || !*list || !(*list)->next)
     return;
 
   while (swapped)
   {
     swapped = 0;
-    
+    start = *list;
+
     while (start->next != end)
     {
       if (start->n > start->next->n)
@@ -48,16 +49,16 @@ void cocktail_sort_list(listint_t **list)
         swap_nodes(list, start, start->next);
         swapped = 1;
       }
-      start = start->next;
+      else
+      {
+        start = start->next;
+      }
     }
 
+    end = start;
     if (!swapped)
       break;
-
     swapped = 0;
-    end = start;
-    start = start->prev;
-
     while (start->prev != NULL)
     {
       if (start->n < start->prev->n)
@@ -65,7 +66,10 @@ void cocktail_sort_list(listint_t **list)
         swap_nodes(list, start->prev, start);
         swapped = 1;
       }
-      start = start->prev;
+      else
+      {
+        start = start->prev;
+      }
     }
   }
 }
